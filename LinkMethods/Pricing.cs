3c701f792ee6a3defd758bdfdd45eb6402b9effb
max-student <max-student@maxtrain.com>
2020-02-21 15:43:27 -0500

    Capstone Method Syntax and Declarative Syntax

---
+++ LinkMethods/Pricing.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkMethods {
+
+    public class Pricing {
+
+        public string ProductCode { get; set; }
+        public double Price { get; set; }
+    }
+
+}
+
--- LinkMethods/Program.cs
+++ LinkMethods/Program.cs
@@ -4,7 +4,42 @@ using System.Linq;
 namespace LinkMethods {
     class Program {
         static void Main(string[] args) {
-            int[] nbrs2 = {
+
+            Product[] productList = {
+                new Product { Code = "Echo", Name = "Amazon Echo", Quantity = 3, Taxible = true },
+                new Product { Code = "EchoDot", Name = "Amazon Echo Dot", Quantity = 7, Taxible = true },
+                new Product { Code = "EchoShow5", Name = "Amazon Echo Show 5", Quantity = 5, Taxible = true },
+                new Product { Code = "3Way", Name = "Skyline 3-Way", Quantity = 2, Taxible = false },
+                new Product { Code = "4Way", Name = "Skyline 4-Way", Quantity = 1, Taxible = false },
+                new Product { Code = "5Way", Name = "Skyline 5-Way", Quantity = 2, Taxible = false },
+                new Product { Code = "5DayClass", Name = "MAX 5 day clas", Quantity = 6, Taxible = true }
+            };
+            Pricing[] priceList = {
+                new Pricing { ProductCode = "Echo", Price = 99.99 },
+                new Pricing { ProductCode = "EchoDot", Price = 39.99 },
+                new Pricing { ProductCode = "EchoShow5", Price = 119.99 },
+                new Pricing { ProductCode = "3Way", Price = 6.99 },
+                new Pricing { ProductCode = "4Way", Price = 7.99 },
+                new Pricing { ProductCode = "5Way", Price = 8.99 },
+                new Pricing { ProductCode = "5DayClass", Price = 2995.00 }
+            };
+
+            var ProductsWithPrices = from prod in productList
+                                     join pric in priceList
+                                     on prod.Code equals pric.ProductCode
+                                     select new {
+                                         Code = prod.Code,
+                                         Name = prod.Name,
+                                         Price = pric.Price,
+                                         Quantity = prod.Quantity,
+                                         Total = prod.Quantity * pric.Price
+                                     };
+           foreach(var p in ProductsWithPrices) {
+                Console.WriteLine($"{p.Code} | {p.Name} | {p.Price} | {p.Quantity} | {p.Total}");
+            }
+
+            /*
+             int[] nbrs2 = {
                 3524,    7436,    7815,    8881,    4972,    4964,
                 5662,    1106,    7676,    1806,    4933,    5317,
                 1557,    9323,    4655,    4389,    2562,    8428,
@@ -17,11 +52,21 @@ namespace LinkMethods {
             };
 
             var total = nbrs2.Sum();
-            Console.WriteLine(total);
+            Console.WriteLine($"Total is {total}");
 
             var total3 = nbrs2.Where(nbrs2 => nbrs2 % 3 == 0).Sum();
             Console.WriteLine($"Numbers divided by 3 = {total3}");
 
+            var avg = nbrs2.Average();
+            Console.WriteLine($"Average is {avg}");
+
+            var total3Extended = from n fred variable in nbrs2
+                                 where n % 3 == 0
+                                 select n;
+            foreach(var n in total3Extended) {
+                Console.WriteLine(n);
+            }
+            */
 
         }
     }
